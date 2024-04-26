0f481baa92ac4a8b083f3ebe91a9c46c6d971a25
arkalal <arkalal.123@gmail.com>
2024-04-26 20:25:44 +0530

    finished responsive

--- components/screens/Home/Home.js
+++ components/screens/Home/Home.js
@@ -3,6 +3,7 @@ import Navbar from "../../reuse/Navbar/Navbar";
 import Banner from "../../Banner/Banner";
 import Intro from "../../Intro/Intro";
 import Footer from "../../Footer/Footer";
+import ChatAssistants from "../../reuse/ChatAssistants/ChatAssistants";
 
 const Home = () => {
   return (
@@ -11,6 +12,8 @@ const Home = () => {
       <Banner />
       <Intro />
       <Footer />
+
+      <ChatAssistants />
     </div>
   );
 };
