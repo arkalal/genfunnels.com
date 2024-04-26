d032be2c683822f12628a7f098dfa16d8010bd92
arkalal <arkalal.123@gmail.com>
2024-04-26 16:26:42 +0530

    finished landing page

--- components/screens/Home/Home.js
+++ components/screens/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../reuse/Navbar/Navbar";
 import Banner from "../../Banner/Banner";
 import Intro from "../../Intro/Intro";
+import Footer from "../../Footer/Footer";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Banner />
       <Intro />
+      <Footer />
     </div>
   );
 };
